922a73eda5da9de65adc6321353a128b9e24e257
Zach Renner <zarenner@microsoft.com>
2019-05-08 12:55:50 -0700

    stuff

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azurepipelinesbuildtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
